55dfc966c600d098791ce863cb5ca6c20af12ba5
Chris Sturm <chris.sturm@gmail.com>
2021-01-10 21:40:45 -0800

    Adding a license
    Chose BSD-3 for now.

--- app/forms.py
+++ app/forms.py
@@ -1,3 +1,14 @@
+"""
+Rally Call 
+
+This app is designed to be a backup communication hub which allows persons to call in and 
+either leave a voice memo or listen to voice memos from other members of their group. 
+
+Copyright 2021 Chris Sturm <chris.sturm@gmail.com>
+
+This application is licensed under the terms of BSD-3.
+Please refer to LICENSE in the project repository for details.
+"""
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, ValidationError, Length, Regexp, Optional
--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,14 @@
+"""
+Rally Call 
+
+This app is designed to be a backup communication hub which allows persons to call in and 
+either leave a voice memo or listen to voice memos from other members of their group. 
+
+Copyright 2021 Chris Sturm <chris.sturm@gmail.com>
+
+This application is licensed under the terms of BSD-3.
+Please refer to LICENSE in the project repository for details.
+"""
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import exc, or_, desc
